32d1b3f70ad602d5fc77f1427d7ebe7bd704e5b5
Kuss <kussssss@outlook.com>
2022-06-30 13:49:41 +0800

    feat: http protocol prefix for localhost

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const WebSocketServer = require('./lib/WebSocketServer');
 
 function start({ port = 8080, host, domain, server } = {}) {
   domain = domain || 'localhost:' + port;
+  const prefix = domain.startsWith('localhost') ? 'http://' : ''
 
   const app = new Koa();
   const wss = new WebSocketServer();
@@ -17,7 +18,7 @@ function start({ port = 8080, host, domain, server } = {}) {
     server.on('request', app.callback());
     wss.start(server);
   } else {
-    util.log(`starting server at ${domain}`);
+    util.log(`starting server at ${prefix}${domain}`);
     const server = app.listen(port, host);
 
     wss.start(server);
